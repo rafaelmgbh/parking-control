01c79c7d14dc2ad3c350243a4121b5874c488f8f
Rafael Santos de Araujo <rafaelsantosdearaujo@gmail.com>
2022-10-19 15:44:32 -0300

    created method PUT

--- src/main/java/com/api/parkingcontrol/controllers/ParkingSpotControler.java
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotControler.java
@@ -67,4 +67,15 @@ public class ParkingSpotControler {
         parkingSpotService.delete(parkingSpotModelOptional.get());
         return ResponseEntity.status(HttpStatus.OK).body("Parking spot deleted");
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateParkingSpot(@PathVariable(value = "id") UUID id, @RequestBody @Valid ParkingSpotDto parkingSpotDto){
+        Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
+        if(!parkingSpotModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking spot not found");
+        }
+        var parkingSpotModel = parkingSpotModelOptional.get();
+        BeanUtils.copyProperties(parkingSpotDto, parkingSpotModel);
+        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.save(parkingSpotModel));
+    }
 }
